8fbc77d1563c845b62d9f76c43a29e0d3e7480bd
urke90 <urosbijelic90@gmail.com>
2021-12-12 11:38:11 +0100

    added car button component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,13 +2,14 @@ import React, { Fragment } from 'react';
 
 import mealsImg from '../../assets/meals.jpg';
 import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = () => {
     return (
         <Fragment>
             <header className={classes.header}>
                 <h1>Meal App</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImg} alt="Order your food" />
