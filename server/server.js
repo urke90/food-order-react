c30623ad281cc6958f973180622014bb6ae2a853
urke90 <urosbijelic90@gmail.com>
2022-01-03 15:03:39 +0100

    added auth routes and user model

--- server/server.js
+++ server/server.js
@@ -1,26 +1,44 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDbStore = require('connect-mongodb-session')(session);
 
-const MONGO_DB_URL =
-    'mongodb+srv://urke90:cikaKure1990@cluster0.yqbde.mongodb.net/food-order-app?retryWrites=true&w=majority';
+const MONGODB_URI =
+    'mongodb+srv://urke90:cikaKure1990@cluster0.yqbde.mongodb.net/food-order-app';
 
 const app = express();
+const store = new MongoDbStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+});
 
 // const mongoConnect = require('./util/database').mongoConnect;
-const mealRoutes = require('./routes/meal');
 
 const port = process.env.PORT || 3001;
 
 // cors ==> removes cors errors when making request to Node/Express app
-app.use(cors());
 // allows parsing JSON
+// express-session is pkg allowing us to use sessions
+app.use(cors());
 app.use(express.json());
+app.use(
+    session({
+        secret: 'my secret string',
+        resave: false,
+        saveUninitialized: false,
+        store: store
+    })
+);
 
+const mealRoutes = require('./routes/meal');
+const authRoutes = require('./routes/auth');
+
+app.use(authRoutes);
 app.use('/meals', mealRoutes);
 
 mongoose
-    .connect(MONGO_DB_URL)
+    .connect(MONGODB_URI)
     .then((res) => {
         app.listen(port);
     })
